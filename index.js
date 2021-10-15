a6a8b0c286f918e74887db65667847a31ebf4461
Khánh Vi Nguyễn <vicntt10@gmail.com>
2021-10-15 17:28:34 +0700

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + "/index.html"));
 const server = require('http').Server(app)
 require('./app/routes/home')(app);
 require('./app/routes/dmphanloai')(app);
