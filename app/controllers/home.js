21d954e79b0616804947978e493de6e8874b9350
Khánh Vi Nguyễn <vicntt10@gmail.com>
2021-10-15 11:13:03 +0700

    fix index.html

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,3 +2,6 @@ exports.home = function (req, res) {
     res.sendFile(__dirname.replace('app\\controllers', '') + '/index.html')
 }
 
+exports.about = function (req, res) {
+    res.send("Hello home about page")
+}
